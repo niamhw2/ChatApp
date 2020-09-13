0d4986e8635e516ff075ab5be21a1d6574ae9257
niamhw2 <niamhw2@hotmail.com>
2020-09-13 22:00:07 +0100

    Functional Chat app based on spring mvc

--- src/main/java/com/udacity/jwdnd/c1/review/controller/LoginController.java
+++ src/main/java/com/udacity/jwdnd/c1/review/controller/LoginController.java
@@ -1,15 +1,31 @@
 package com.udacity.jwdnd.c1.review.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping
 public class LoginController {
 
-    @GetMapping()
+    @GetMapping("/login")
     public String loginView() {
         return "login";
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
     }
 }
